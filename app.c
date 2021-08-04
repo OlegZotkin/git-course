0855b5dd56ff0939d79bd4573a6a95b6c837d8ac
OlegZ <allegka86@gmail.com>
2021-08-04 21:33:53 +0300

    new code

--- app.c
+++ app.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
-#include <stdbool.h>
 
 int main(void) {
-    int i;
-    bool isFlag = true;
+    void scalarMultiply(int nRows, int nCols, int matrix[nRows][nCols], int scalar);
+    void displayMatrix(int nRows, int cCols, int matrix[nRows][nCols]);
+    int  sampleMatrix[3][5] = {
+        { 7, 16, 55, 13, 12 },
+        { 12, 10, 52, 0, 7 },
+        { -2, 1, 2, 4, 9 }
+    };
+    printf("Original matrix:\n");
+    displayMatrix(3, 5, sampleMatrix);
+    scalarMultiply(3, 5, sampleMatrix, 2);
+    printf("\nMultiplied by 2:\n");
+    displayMatrix(3, 5, sampleMatrix);
+    scalarMultiply(3, 5, sampleMatrix, -1);
+    printf("\nThen multiplied by -1:\n");
+    displayMatrix(3, 5, sampleMatrix);
 
-    printf("Good");
-    printf("\n");
     return 0;
 }
